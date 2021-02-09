99e163582339d7e93658df4f0bb67e14d8d2ee68
michael <mpthornton86@gmail.com>
2021-02-09 14:03:01 -0700

    commit tree 9da16d59fb297702416ad5cf0b4245df757c05f4

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let lookup = arr.find( year => year.result === 'W' )
+  return !!lookup ? lookup.year : undefined
+}
